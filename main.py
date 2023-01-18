862e5f15e76f0f44eb28e74b3b9fdc964e31c53b
PoojaGaur22 <poojagaur2293@gmail.com>
2023-01-18 19:32:40 +0530

    complete code

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
         cmd_list: List[str] = command.split(' ')
 
         if cmd_list[0] == "create":
-            utils.create_user()
+            utils.create_user(cmd_list)
         elif cmd_list[0] == "add":
-            utils.add_habit_for_user()
+            utils.add_habit_for_user(cmd_list)
         elif cmd_list[0] == "record":
-            utils.record_data_in_habit()
+            utils.record_data_in_habit(cmd_list)
         elif cmd_list[0] == "get":
-            utils.get_data()
+            utils.get_data(cmd_list)
         elif cmd_list[0] == "help":
             utils.help_msg()
         elif cmd_list[0] == "exit":
--- utils.py
+++ utils.py
@@ -1,16 +1,87 @@
 import sys
+from typing import List
+import requests
 
-def create_user():
-    pass
 
-def add_habit_for_user():
-    pass
+def generate_graph_id(username: str, habit: str):
+    return f"{username}-{habit}"[0:16]
 
-def record_data_in_habit():
-    pass
 
-def get_data():
-    pass
+def create_user(cmd_list: List):
+    URL = "https://pixe.la/v1/users"
+
+    request_body = {
+        "username": cmd_list[1],
+        "token": "issecret",
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes"
+        }
+
+    response = requests.post(url=URL, json=request_body)
+
+    if response.status_code == 200:
+        print(f'message -> {response.json()["message"]}')
+        print("User created successfully")
+    else:
+        print(f'Status code -> {response.status_code}')
+        print("Try again!")
+
+
+def add_habit_for_user(cmd_list: List):
+    URL = f'https://pixe.la/v1/users/{cmd_list[1]}/graphs'
+
+    request_body = {
+        "id": generate_graph_id(cmd_list[1], cmd_list[2]),
+        "name": cmd_list[2],
+        "unit": "commit",
+        "type": "int",
+        "color": "ajisai",
+    }
+
+    header = {
+        "X-USER-TOKEN": "issecret"
+    }
+
+    response = requests.post(url=URL, headers=header, json=request_body)
+
+    if response.status_code == 200:
+        print(f'message -> {response.json()["message"]}')
+        print("Graph created successfully")
+    else:
+        print(f'Status code -> {response.status_code}')
+        print("Try again!")
+
+
+def record_data_in_habit(cmd_list: List):
+
+    graph_id = generate_graph_id(cmd_list[1], cmd_list[2])
+    URL = f"https://pixe.la/v1/users/{cmd_list[1]}/graphs/{graph_id}"
+
+    request_header = {
+        "X-USER-TOKEN": "issecret"
+    }
+
+    request_body = {
+        "date": cmd_list[3],
+        "quantity":"1"
+    }
+
+    response = requests.post(url=URL, headers=request_header, json=request_body)
+
+    if response.status_code == 200:
+        print(f'message -> {response.json()["message"]}')
+        print("Entry added in habit graph")
+    else:
+        print(f'Status code -> {response.status_code}')
+        print("Try again!")
+
+
+def get_data(cmd_list: List):
+    graph_id = generate_graph_id(cmd_list[1], cmd_list[2])
+
+    URL = f"https://pixe.la/v1/users/{cmd_list[1]}/graphs/{graph_id}.html"
+
+    print(f"Hi, Please check your habit graph on this link ->{URL}")
 
 
 def help_msg():
